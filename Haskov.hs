56f817c2526ac4ef626f47676fb024380245c5b2
Mark Zuschlag <markazuschlag@gmail.com>
2017-03-22 06:51:13 +0000

    unstable walk function

--- Haskov.hs
+++ Haskov.hs
@@ -8,75 +8,101 @@ import qualified Data.Map.Strict as Map
 import Data.Vector (Vector, (!?))
 import qualified Data.Vector as Vec
 
--- HaskovMatrix Type --
-data HaskovMatrix a = HaskovMatrix { hmap :: Map a Int
+-- Markov Type --
+data Markov a = Markov { hmap :: Map a Int
                                      , hmatrix :: Matrix Double }
                                      
-instance (Show a, Ord a) => Show (HaskovMatrix a) where 
+instance (Show a, Ord a) => Show (Markov a) where 
     show haskov = show $ toList haskov 
 
 -- Query --
-lookUp :: (Ord a) => a -> a -> HaskovMatrix a -> Maybe Double
-lookUp i j (HaskovMatrix hmap hmatrix)
+lookUp :: (Ord a) => a -> a -> Markov a -> Maybe Double
+lookUp i j (Markov hmap hmatrix)
     | hasI && hasJ = Just (getElem (hmap ! i) (hmap ! j) hmatrix)
     | otherwise    = Nothing
     where
         hasI = Map.member i hmap
         hasJ = Map.member j hmap
 
-size :: (Ord a) => HaskovMatrix a -> Int
-size (HaskovMatrix hmap hmatrix) = Map.size hmap
+size :: (Ord a) => Markov a -> Int
+size (Markov hmap hmatrix) = Map.size hmap
 
-null :: (Ord a) => HaskovMatrix a -> Bool
-null (HaskovMatrix hmap hmatrix) = Map.null hmap 
+null :: (Ord a) => Markov a -> Bool
+null (Markov hmap hmatrix) = Map.null hmap 
 
-member :: (Ord a) => a -> a -> HaskovMatrix a -> Bool
-member i j (HaskovMatrix hmap hmatrix)
+member :: (Ord a) => a -> a -> Markov a -> Bool
+member i j (Markov hmap hmatrix)
     | (Map.member i hmap) && (Map.member j hmap) = True
     | otherwise = False
 
-notMember :: (Ord a) => a -> a -> HaskovMatrix a -> Bool
+notMember :: (Ord a) => a -> a -> Markov a -> Bool
 notMember i j haskov = not (member i j haskov)
 
-states :: (Ord a) => HaskovMatrix a -> [a]
-states (HaskovMatrix hmap hmatrix) = keys hmap
+states :: (Ord a) => Markov a -> [a]
+states (Markov hmap hmatrix) = keys hmap
 
 -- Construction --         
-empty :: HaskovMatrix a
-empty = HaskovMatrix (Map.empty) (Mat.zero 0 0)
+empty :: Markov a
+empty = Markov (Map.empty) (Mat.zero 0 0)
 
-singleton :: (Ord a) => a -> Double -> HaskovMatrix a
+singleton :: (Ord a) => a -> Double -> Markov a
 singleton a n = 
-    HaskovMatrix (Map.singleton a 1) (Mat.matrix 1 1 (\(i,j) -> n))
+    Markov (Map.singleton a 1) (Mat.matrix 1 1 (\(i,j) -> n))
 
 -- insert --
-insert :: (Ord a) => a -> a -> Double -> HaskovMatrix a -> HaskovMatrix a
-insert i j n (HaskovMatrix hmap hmatrix) =
+insert :: (Ord a) => a -> a -> Double -> Markov a -> Markov a
+insert i j n (Markov hmap hmatrix) =
     let newMap = hmapInsert i j hmap
-    in HaskovMatrix (newMap) (hmatrixInsert i j n hmatrix newMap)
+    in Markov (newMap) (hmatrixInsert i j n hmatrix newMap)
 
 -- Chains -- 
-walk :: (Ord a) => HaskovMatrix a -> Int -> IO [a]
+walk :: (Ord a) => Markov a -> Int -> IO [a]
 walk haskov n = do 
     gen <- getStdGen
     let rand = randomR (0, (size haskov) - 1) gen :: (Int, StdGen)
         start = Vec.fromList (states haskov) !? (fst rand)
     return (walker haskov n start (snd rand))
 
-walker :: (Ord a) => HaskovMatrix a -> Int -> Maybe a -> StdGen -> [a]
+walker :: (Ord a) => Markov a -> Int -> Maybe a -> StdGen -> [a]
 walker _ _ Nothing _ = []
-walker (HaskovMatrix hmap hmatrix) 0 s gen = []
-walker (HaskovMatrix hmap hmatrix) n s gen = []
-    --let rand = randomR
+walker _ 0 _ _ = []
+walker (Markov hmap hmatrix) n (Just s) gen =
+    let row = Mat.getRow (hmap ! s) hmatrix
+        rand = randomR (0, 1000000000) gen :: (Double, StdGen)
+        prob = (fst rand) * 0.000000001
+        choice = Vec.foldl (\acc x -> if (abs x) < acc then x else acc) 1.0 (Vec.map (subtract prob) row)
+        options = Vec.filter ((\x y -> (snd y) == x) prob) (Vec.zip (Vec.fromList . keys $ hmap) (Vec.map (subtract choice) row))
+        index = randomR (0, (Vec.length options)-1) (snd rand) :: (Int, StdGen)
+        value = fst (options Vec.! (fst index))
+    in value : walker (Markov hmap hmatrix) (n-1) (Just value) (snd index) 
+
+testwalk :: (Ord a) => Markov a -> Int -> IO [a]
+testwalk haskov n = do 
+    gen <- getStdGen
+    let rand = randomR (0, (size haskov) - 1) gen :: (Int, StdGen)
+        start = Vec.fromList (states haskov) !? 0
+    return (testwalker haskov n start (snd rand))
+
+testwalker :: (Ord a) => Markov a -> Int -> Maybe a -> StdGen -> [a]
+testwalker (Markov hmap hmatrix) n (Just s) gen =
+    let row = Mat.getRow (hmap ! s) hmatrix
+        rand = randomR (0, 1000000000) gen :: (Double, StdGen)
+        --prob = (fst rand) * 0.000000001
+        prob = 0.6
+        choice = Vec.foldl (\acc x -> if (abs x) < acc then x else acc) 1.0 (Vec.map (subtract prob) row)
+        options = Vec.filter ((\x y -> (snd y) == x) prob) (Vec.zip (Vec.fromList . keys $ hmap) (Vec.map (subtract choice) row))
+        index = randomR (0, (Vec.length options)-1) (snd rand) :: (Int, StdGen)
+        value = fst (options Vec.! (fst index))
+    in value : testwalker (Markov hmap hmatrix) (n-1) (Just value) (snd index) 
 
 -- Lists --    
-toList :: (Ord a) => HaskovMatrix a -> [((a, a), Double)]
+toList :: (Ord a) => Markov a -> [((a, a), Double)]
 toList haskov =
     let pairs = rowColPairs haskov
         values = map (rowColValue haskov) pairs
     in zip pairs values
 
-fromList :: (Ord a) => [((a, a), Double)] -> HaskovMatrix a
+fromList :: (Ord a) => [((a, a), Double)] -> Markov a
 fromList [] = empty
 fromList tuples = foldl (\acc x -> insert (fst . fst $ x) (snd . fst $ x) (snd x) acc) empty tuples
         
@@ -102,14 +128,14 @@ hmatrixInsert i j n hmatrix hmap
         mapSize = Map.size hmap
         matrixSize = Mat.nrows hmatrix
 
-showPairs :: (Ord a, Show a) => HaskovMatrix a -> String
+showPairs :: (Ord a, Show a) => Markov a -> String
 showPairs haskov = show . rowColPairs $ haskov
 
-rowColValue :: (Ord a) => HaskovMatrix a -> (a, a) -> Double
-rowColValue (HaskovMatrix hmap hmatrix) (i, j) =
+rowColValue :: (Ord a) => Markov a -> (a, a) -> Double
+rowColValue (Markov hmap hmatrix) (i, j) =
     getElem (hmap ! i) (hmap ! j) hmatrix 
 
-rowColPairs :: (Ord a) => HaskovMatrix a -> [(a, a)]
-rowColPairs (HaskovMatrix hmap hmatrix) = 
+rowColPairs :: (Ord a) => Markov a -> [(a, a)]
+rowColPairs (Markov hmap hmatrix) = 
     [(i, j) | i <- (keys hmap), j <- (keys hmap)]
     
